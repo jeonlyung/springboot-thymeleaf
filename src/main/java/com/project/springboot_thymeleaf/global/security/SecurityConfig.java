6454ef892ebff672f4f50500f3b69ed420d68089
jeonlyung <bindongx2@naver.com>
2026-01-13 18:11:34 +0900

    Spring Security 예외처리

---
+++ src/main/java/com/project/springboot_thymeleaf/global/security/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.project.springboot_thymeleaf.global.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeHttpRequests(auth -> auth.anyRequest()
+                .permitAll())
+                .csrf(csrf -> csrf.disable());
+
+        return httpSecurity.build();
+    }
+}
