e163fb30ed189fca3e5598c132580f62a3a6bf10
jeonlyung <bindongx2@naver.com>
2026-01-16 13:00:06 +0900

    상수 Constants.java 파일 추가

--- src/main/java/com/project/springboot_thymeleaf/global/constatns/Constants.java
+++ src/main/java/com/project/springboot_thymeleaf/global/constatns/Constants.java
@@ -1,4 +1,10 @@
 package com.project.springboot_thymeleaf.global.constatns;
 
 public class Constants {
+
+    public static final String SUCCESS_Y = "Y";
+    public static final String SUCCESS_N = "N";
+    public static final boolean BOOLEAN_TRUE = true;
+    public static final boolean BOOLEAN_FALSE = false;
+
 }
