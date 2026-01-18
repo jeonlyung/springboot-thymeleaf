9b7d8f81579f61d41527802ac26bc48d81fd6b1f
jeonlyung <bindongx2@naver.com>
2026-01-18 20:17:14 +0900

    BatchExecutor 공통 배치 함수 추가

---
+++ src/main/java/com/project/springboot_thymeleaf/global/batch/BatchExecutor.java
@@ -0,0 +1,35 @@
+package com.project.springboot_thymeleaf.global.batch;
+
+import org.apache.ibatis.session.ExecutorType;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class BatchExecutor {
+    @Autowired
+    SqlSessionFactory sqlSessionFactory;
+
+    public <M, T> void executeBatch(Class<M> mapperClass, List<T> dsList){
+        int batchSize = 1000;
+        int count = 0;
+        try(SqlSession sqlSession = sqlSessionFactory.openSession(ExecutorType.BATCH, false)){
+            M mapper = sqlSession.getMapper(mapperClass);
+
+            for (T data : dsList){
+                if (++count % batchSize == 0) {
+                    sqlSession.flushStatements(); // 중간 커밋 효과 (메모리 관리)
+                    sqlSession.clearCache();
+                }
+            }
+
+            sqlSession.flushStatements();
+            sqlSession.clearCache();
+            sqlSession.commit();
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
