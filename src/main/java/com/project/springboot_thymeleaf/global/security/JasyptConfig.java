9accd1194865459e1ce0c509098a238d757a7f48
jeonlyung <bindongx2@naver.com>
2026-01-22 15:50:29 +0900

    Jasypt 암호화 라이브러리 및 로직 변경  - @Value 어노테이션 사용하도록 편리 제공

--- src/main/java/com/project/springboot_thymeleaf/global/security/JasyptConfig.java
+++ src/main/java/com/project/springboot_thymeleaf/global/security/JasyptConfig.java
@@ -1,20 +1,27 @@
 package com.project.springboot_thymeleaf.global.security;
 
 import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
+import org.jasypt.spring31.properties.EncryptablePropertySourcesPlaceholderConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class JasyptConfig {
 
+    // 1. 실제 복호화를 담당하는 엔진 Bean
     @Bean("jasyptStringEncryptor")
     public StandardPBEStringEncryptor jasyptStringEncryptor() {
         StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
-        String password = System.getProperty("jasypt.password");
-
-        encryptor.setPassword(password);
-        encryptor.setAlgorithm("PBEWithMD5AndDES"); // 기본 알고리즘
-
+        // 실행 시 -Djasypt.password=... 로 주입
+        encryptor.setPassword(System.getProperty("jasypt.password", "default_key"));
+        encryptor.setAlgorithm("PBEWithMD5AndDES");
         return encryptor;
+    }
+
+    // 2. ${} 안에 ENC()가 있으면 자동으로 복호화해서 교체해주는 Bean (핵심)
+    @Bean
+    public static EncryptablePropertySourcesPlaceholderConfigurer propertyConfigurer(StandardPBEStringEncryptor jasyptStringEncryptor) {
+        // org.jasypt.spring31... 패키지에서 제공하는 클래스입니다.
+        return new EncryptablePropertySourcesPlaceholderConfigurer(jasyptStringEncryptor);
     }
 }
