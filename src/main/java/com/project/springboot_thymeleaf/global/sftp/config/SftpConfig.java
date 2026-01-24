8cfd592fae40025c85092d141f8ed941a028c93b
jeonlyung <bindongx2@naver.com>
2026-01-24 17:21:11 +0900

    Sftp Host 다중처리

--- src/main/java/com/project/springboot_thymeleaf/global/sftp/config/SftpConfig.java
+++ src/main/java/com/project/springboot_thymeleaf/global/sftp/config/SftpConfig.java
@@ -5,14 +5,16 @@ import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.List;
+
 @Configuration
 @ConfigurationProperties(prefix = "sftp")
 @Getter @Setter
 public class SftpConfig {
-    private String host;      // yml의 host
-    private int port;         // yml의 port
-    private String user;      // yml의 user
-    private String password;  // yml의 password
-    private String directory; // yml의 directory
+    private List<String> hosts; // yml의 host
+    private int port;           // yml의 port
+    private String user;        // yml의 user
+    private String password;    // yml의 password
+    private String directory;   // yml의 directory
 }
 
