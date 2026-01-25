8fd92b4b8efdaf6381463cd84b530c51404fe611
jeonlyung <bindongx2@naver.com>
2026-01-25 11:15:41 +0900

    ImageUtil 주석 추가

--- src/main/java/com/project/springboot_thymeleaf/global/util/ImageUtil.java
+++ src/main/java/com/project/springboot_thymeleaf/global/util/ImageUtil.java
@@ -4,6 +4,7 @@ import net.coobird.thumbnailator.Thumbnails;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 
@@ -21,18 +22,18 @@ public class ImageUtil {
             throw new IllegalArgumentException("파일이 비어있습니다.");
         }
 
-        // 2. 최적화 처리 (Java 21의 리소스 관리 기능을 활용)
+        // 2. 최적화 처리
         try (InputStream is = mFile.getInputStream();
-             ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
-
+             ByteArrayOutputStream os = new ByteArrayOutputStream()) {
 
             Thumbnails.of(is)
-                    .size(1920, 1080)     // FHD 해상도로 리사이징 (비율 유지)
+                    .size(1920, 1080)  // FHD 해상도로 리사이징 (비율 유지)
                     .outputQuality(0.8)   // 화질 80% (용량 절감의 핵심)
-                    .outputFormat("jpg")  // PNG/HEIC보다 가벼운 JPG로 변환
-                    .toOutputStream(baos);
+                    .imageType(BufferedImage.TYPE_INT_RGB) // 불필요한 투명 채널 제거 (용량 추가 절감)
+                    .outputFormat("jpg")  // 메타데이터 제거 및 JPG로 변환
+                    .toOutputStream(os);
 
-            return baos.toByteArray();
+            return os.toByteArray();
         }
     }
 }
