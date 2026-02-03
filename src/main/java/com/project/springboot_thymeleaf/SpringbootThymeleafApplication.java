c507d29a88dda948b9a5bd7e3630269f419f5340
jeonlyung <bindongx2@naver.com>
2026-02-03 17:16:09 +0900

    DB연결 테스트(정상) 완료

--- src/main/java/com/project/springboot_thymeleaf/SpringbootThymeleafApplication.java
+++ src/main/java/com/project/springboot_thymeleaf/SpringbootThymeleafApplication.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class}) // DB 설정 제외
+//@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class}) // DB 설정 제외
+@SpringBootApplication
 public class SpringbootThymeleafApplication {
 
 	public static void main(String[] args) {
