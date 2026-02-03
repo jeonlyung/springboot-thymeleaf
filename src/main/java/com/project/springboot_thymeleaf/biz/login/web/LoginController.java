02eee4b6111425d184d6ce1896c7f6bfa84b18a1
jeonlyung <bindongx2@naver.com>
2026-02-03 18:04:18 +0900

    LoginController 추가

---
+++ src/main/java/com/project/springboot_thymeleaf/biz/login/web/LoginController.java
@@ -0,0 +1,21 @@
+package com.project.springboot_thymeleaf.biz.login.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/login")
+public class LoginController {
+
+    @GetMapping("/")
+    public String goLogin() {
+
+        return "login/login";
+    }
+
+    @PostMapping("/processLogin")
+    public String processLogin() {
+
+        return "login/login";
+    }
+}
