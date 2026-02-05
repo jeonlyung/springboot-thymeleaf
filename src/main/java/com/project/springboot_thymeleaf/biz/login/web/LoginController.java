57e332f5a8f8c3161924b8c94c750877a98811b5
jeonlyung <bindongx2@naver.com>
2026-02-05 17:55:53 +0900

    LoginController 추가

--- src/main/java/com/project/springboot_thymeleaf/biz/login/web/LoginController.java
+++ src/main/java/com/project/springboot_thymeleaf/biz/login/web/LoginController.java
@@ -1,13 +1,15 @@
 package com.project.springboot_thymeleaf.biz.login.web;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 @RequestMapping("/login")
 public class LoginController {
 
-    @GetMapping("/")
+    @GetMapping("")
     public String goLogin() {
         return "login/login";
     }
--- src/main/java/com/project/springboot_thymeleaf/biz/login/web/OauthController.java
+++ src/main/java/com/project/springboot_thymeleaf/biz/login/web/OauthController.java
@@ -1,8 +1,10 @@
 package com.project.springboot_thymeleaf.biz.login.web;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 @RequestMapping("/oauth")
 public class OauthController {
 
