5c557aab59442092a6bd866a1aa9754bac4fbb61
jeonlyung <bindongx2@naver.com>
2026-02-07 09:37:26 +0900

    일반 로그인 시큐어리티 기능 추가

--- src/main/java/com/project/springboot_thymeleaf/global/security/SecurityConfig.java
+++ src/main/java/com/project/springboot_thymeleaf/global/security/SecurityConfig.java
@@ -19,14 +19,23 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.csrf(csrf -> csrf.disable())
                 .headers(headers -> headers.frameOptions(frame -> frame.disable()))
-                // 2. 권한 설정(좁은 범위부터 넓은 범위로 설정해야함)
+                // 권한 설정(좁은 범위부터 넓은 범위로 설정해야함)
                 .authorizeHttpRequests(auth -> auth
                         // 정적 리소스 및 로그인 관련 경로는 누구나 접근 가능
                         .requestMatchers("/", "/login/**", "/css/**", "/js/**", "/images/**").permitAll()
                         // 그 외 모든 요청은 인증 필요
                         .anyRequest().authenticated()
                 )
-                // 3. OAuth2 로그인 설정
+                // 일반 로그인(ID/PW) 설정
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .loginProcessingUrl("/login/process")
+                        .usernameParameter("username")
+                        .passwordParameter("password")
+                        .defaultSuccessUrl("/main", true) // 성공하면 바로 메인으로
+                        .permitAll()
+                )
+                // OAuth2 로그인 설정
                 .oauth2Login(oauth2 -> oauth2
                         .loginPage("/login")
                         .defaultSuccessUrl("/main", true)
@@ -34,7 +43,7 @@ public class SecurityConfig {
                                 .userService((oauthService))
                         )
                 )
-                // 4. 로그아웃 설정
+                // 로그아웃 설정
                 .logout(logout -> logout
                         .logoutSuccessUrl("/")
                         .invalidateHttpSession(true)
