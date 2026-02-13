f33e18144f59da30840f073267b1ad27b42aac75
jeonlyung <bindongx2@naver.com>
2026-02-13 10:55:26 +0900

    어노테이션 누락 수정

--- src/main/java/com/project/springboot_thymeleaf/biz/MainController.java
+++ src/main/java/com/project/springboot_thymeleaf/biz/MainController.java
@@ -1,7 +1,9 @@
 package com.project.springboot_thymeleaf.biz;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class MainController {
 
     @GetMapping("/")
